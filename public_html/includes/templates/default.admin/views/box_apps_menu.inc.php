c8638a6152aa5acbc9d68415c51da56ead8479f1
IBBoard <dev@ibboard.co.uk>
2025-03-29 08:14:38 +0000

    Move ACP section colours to <style>

--- public_html/includes/templates/default.admin/views/box_apps_menu.inc.php
+++ public_html/includes/templates/default.admin/views/box_apps_menu.inc.php
@@ -1,7 +1,12 @@
+<style nonce="<?php echo $_SERVER['CSP_NONCE']; ?>">
+  <?php foreach ($apps as $app) { ?>
+  #box-apps-menu .app.<?php echo $app['code']; ?> { --app-color: <?php echo $app['theme']['color']; ?> }
+  <?php } ?>
+</style>
 <ul id="box-apps-menu">
 
   <?php foreach ($apps as $app) { ?>
-  <li class="app<?php echo $app['active'] ? ' active' : ''; ?>" data-code="<?php echo $app['code']; ?>" style="--app-color: <?php echo $app['theme']['color']; ?>;">
+  <li class="app <?php echo $app['code'] . ($app['active'] ? ' active' : ''); ?>" data-code="<?php echo $app['code']; ?>">
     <a href="<?php echo functions::escape_html($app['link']); ?>" data-toggle="ajax-load">
       <span class="app-icon" title="<?php echo functions::escape_html($app['name']); ?>">
         <?php echo functions::draw_fonticon($app['theme']['icon'] .' fa-fw'); ?>
